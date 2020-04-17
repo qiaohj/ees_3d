3cc884ef8d2222c231fba224a90f1076c925bbd7
Huijie Qiao <huijieqiao@gmail.com>
2020-04-17 17:05:34 +0800

    20200417-1

--- R/smart_species/7.figure_individual_ratio.r
+++ R/smart_species/7.figure_individual_ratio.r
@@ -1,28 +1,63 @@
 library(dplyr)
 library(ggplot2)
+library(Rmisc)
 base<-"~/Downloads"
- 
-if (F){
-  #fix result, add evo_type to the data frame.
-  result<-readRDS(sprintf("%s/Tables/individual_ratio.rda", base))
-  result$Y<-result$Y*-1
-  items<-strsplit(result$LABLE, "_")
-  names(items)<-c(1:length(items))
-  items_df<-bind_rows(items)
-}
+
+
+result<-readRDS(sprintf("%s/Tables/individual_ratio.rda", base))
+result$Y<-result$Y*-1
+head(result)
 mean_df<-result %>%
-  group_by(Y, SUITABLE, NB, DA, EVO_RATIO) %>%
-  summarize(Mean_N_IND = mean(N_IND, na.rm=TRUE),
-            Mean_N_SP = mean(N_SP, na.rm=TRUE),
-            Mean_N_CELL = mean(N_CELL, na.rm=TRUE),
-            SD_N_IND = sd(N_IND),
-            SD_N_SP = sd(N_SP),
-            SD_N_CELL = sd(N_CELL)
+  dplyr::group_by(Y, SUITABLE, NB, DA, EVO_RATIO, EVO_TYPE) %>%
+  dplyr::summarize(Mean_N_IND = mean(N_IND, na.rm=TRUE),
+                   Mean_N_SP = mean(N_SP, na.rm=TRUE),
+                   Mean_N_CELL = mean(N_CELL, na.rm=TRUE),
+                   SD_N_IND = sd(N_IND),
+                   SD_N_SP = sd(N_SP),
+                   SD_N_CELL = sd(N_CELL),
+                   Median_N_IND = quantile(N_IND, na.rm=TRUE, .5),
+                   Median_N_SP = quantile(N_SP, na.rm=TRUE, .5),
+                   Median_N_CELL = quantile(N_CELL, na.rm=TRUE, .5),
+                   CI_N_IND = CI(N_IND, ci=0.95)[2] - CI(N_IND, ci=0.95)[3],
+                   CI_N_SP = CI(N_SP, ci=0.95)[2] - CI(N_SP, ci=0.95)[3],
+                   CI_N_CELL = CI(N_CELL, ci=0.95)[2] - CI(N_CELL, ci=0.95)[3],
+                   N=length(unique(GLOBAL_ID))
   )
 
-mean_df$label<-paste(mean_df$NB, mean_df$DA, mean_df$EVO_RATIO)
 
-ggplot(mean_df)+geom_line(aes(x=Y, y= Mean_N_IND, 
-                              linetype=factor(SUITABLE), color=factor(label)))+
+cols<-c("MODERATE GOOD 0.01 2"="deepskyblue",
+        "MODERATE GOOD 0.1 2"="deepskyblue3",
+        "MODERATE GOOD 0.01 3"="khaki1",
+        "MODERATE GOOD 0.1 3"="khaki3",
+        "MODERATE GOOD 0.01 4"="hotpink",
+        "MODERATE GOOD 0.1 4"="hotpink3",
+        "MODERATE GOOD 1 1"="black")
+
+mean_df[is.na(mean_df)]<-0
+mean_df$label<-paste(mean_df$NB, mean_df$DA, mean_df$EVO_RATIO, mean_df$EVO_TYPE)
+
+mean_df_item<-mean_df %>% filter(NB=="MODERATE" & DA=="GOOD")
+
+ggplot(mean_df_item %>% filter(SUITABLE==0))+geom_line(aes(x=Y, y= N, 
+                                                           color=factor(label)))+
+  theme_bw()+
+  scale_colour_manual(values = cols,aesthetics = c("colour", "fill"))
+
+
+ggplot(mean_df_item)+geom_line(aes(x=Y, y= Median_N_IND, 
+                                   linetype=factor(SUITABLE), color=factor(label)))+
   scale_y_log10()+
   theme_bw()
+
+ggplot(mean_df_item %>% filter(SUITABLE==1 & Y>=-1190), 
+       aes(x=Y, y= Mean_N_CELL, color=factor(label), fill=factor(label)))+
+  geom_line()+
+  geom_ribbon(aes(ymin=Mean_N_CELL-CI_N_CELL, ymax=Mean_N_CELL+CI_N_CELL))+
+  theme_bw()+
+  scale_colour_manual(values = cols,aesthetics = c("colour", "fill"))
+
+ggplot(mean_df_item %>% filter(SUITABLE==1 & Y>=-1190))+geom_line(aes(x=Y, y= Mean_N_SP, 
+                                                                      color=factor(label)))+
+  scale_y_log10()+
+  theme_bw()+
+  scale_colour_manual(values = cols,aesthetics = c("colour", "fill"))
